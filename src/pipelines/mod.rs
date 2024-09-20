fe747ff628ef18aa941677a98a94fd0989f2b92c
= <jhowell45@googlemail.com>
2024-09-20 21:07:59 +0100

    working on the pipeline

--- src/pipelines/mod.rs
+++ src/pipelines/mod.rs
@@ -1,9 +1,8 @@
+use std::rc::Rc;
+
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    recipes::{Recipe, Recipes},
-    resources::Resource,
-};
+use crate::recipes::Recipe;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PipelineNode {
@@ -24,29 +23,7 @@ impl PipelineNode {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Pipeline {
-    root: Box<PipelineNode>,
+    root: Rc<PipelineNode>,
 }
 
-impl Pipeline {
-    pub fn build(resource: Resource, recipes: &Recipes) -> Self {
-        let root = Self::create_node(resource, recipes);
-        if !root.recipe.end {
-            for (input_resource, amount) in root.recipe.input_items.iter() {
-                println!("{:#?}", input_resource);
-                println!("{:#?}", amount);
-            }
-        }
-        Self {
-            root: Box::new(root),
-        }
-    }
-
-    fn create_node(resource: Resource, recipes: &Recipes) -> PipelineNode {
-        let resource_recipes: &Vec<Recipe>;
-        match recipes.get_component_recipes(resource.clone()) {
-            Ok(recipes) => resource_recipes = recipes,
-            Err(msg) => panic!("{} || {:?}", msg, resource),
-        }
-        PipelineNode::new(Box::new(resource_recipes.first().unwrap().clone()))
-    }
-}
+impl Pipeline {}
